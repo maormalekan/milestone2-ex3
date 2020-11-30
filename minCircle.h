558fc847e93a1c9bfdb2b98e093119a37e8a4917
maormalekan <maorsdf@gmail.com>
2020-11-30 19:21:25 +0200

    added documentation for the functions.

--- minCircle.h
+++ minCircle.h
@@ -42,19 +42,37 @@ public:
 /**
  * @brief return the distance of 2 points
  * 
- * @param p1 point
- * @param p2 point
- * @return float distance
+ * @param p1 - point
+ * @param p2 - point
+ * @return float distance between the two points
  */
 float distanceBetweenTwoPoints(const Point& p1, const Point& p2){
 	return sqrt(pow(p1.x-p2.x,2)+pow(p1.y-p2.y,2));
 }
 
+/**
+ * @brief return the circle that two points are generate.
+ * we know that two points can generate only one unique circle.
+ * 
+ * @param p1 - point
+ * @param p2 - point
+ * @return the circle that the two points are generate.
+ */
 Circle getCircleFrom2(const Point& p1, const Point& p2){
 	Point center= {(p1.x+p2.x)/2, (p1.y+p2.y)/2};
 	return Circle(center,distanceBetweenTwoPoints(p1,p2)/2);
 }
 
+/**
+ * @brief return the circle that three points are generate.
+ * we know that three points can generate only one unique circle.
+ * 
+ * @param p1 point
+ * @param p2 - point
+ * @param p3 - point
+ * @return the circle that the three points are generate.
+ * 
+ */
 Circle getCircleFrom3(const Point& p1, const Point& p2, const Point& p3){
 	// cases of slopes of 0 and indefinite - the center of the circle must lie in the 
 	// middle of the diameter
@@ -110,10 +128,25 @@ Circle getCircleFrom3(const Point& p1, const Point& p2, const Point& p3){
 	return Circle(center,R);
 
 }
+
+/**
+ * @brief the function gets a circle and a point, and checks if the point is in the circle.
+ * 
+ * @param c - circle
+ * @param p - point
+ * @return the function returns true if the point is in the circle. otherwise, it returns false.
+ */
 bool pIsInside(const Circle& c, const Point& p){
 	return (distanceBetweenTwoPoints(c.center,p)<=c.radius);
 }
 
+/**
+ * @brief the function gets vector of points that have to be on the edge of the circle,
+ * and returns the minimum circle that include them.
+ * 
+ * @param vec - vector of points
+ * @return the minimum circle that these points are on the edge of it.
+ */
 Circle minCircle(vector<Point> vec){
 		if(vec.size()<=3){
 			if(vec.empty())
@@ -124,7 +157,6 @@ Circle minCircle(vector<Point> vec){
 				return getCircleFrom2(vec[0],vec[1]);
 					
 		}
-
 		// check if we can create a circle by 2 points only
 		for(int i=0;i<3;i++){
 			for(int j=i+1;j<3;j++){
@@ -140,10 +172,20 @@ Circle minCircle(vector<Point> vec){
 					return circ;
 			}
 		}
-      	return getCircleFrom3(vec[0],vec[1],vec[2]);	
-
+      	return getCircleFrom3(vec[0],vec[1],vec[2]);
 }
 
+/**
+ * @brief the recursive function.
+ * the function gets array of points, the size of the array,
+ * and a vector of points that have to be on the edge of the circle.
+ * the function returns the minimun circle that encloses all the points.
+ * 
+ * @param points - the points that have to be in the circle.
+ * @param size - the size of the array of points.
+ * @param vec - the points that have to be on the edge of the circle.
+ * @return the minimun circle that encloses all the points.
+ */
 Circle helper(Point** points,size_t size, vector<Point> vec){
 		if(size==0||vec.size()==3){
 			return minCircle(vec);	
@@ -156,21 +198,19 @@ Circle helper(Point** points,size_t size, vector<Point> vec){
 
 		// else, the point must be on the edge of the circle
 		vec.push_back(p);
-
+		
 		return helper(points,size-1,vec);
-
-
-
 }
 
-
+/**
+ * @brief the main function that calls the recursive function.
+ * 
+ * @param points - an array points that we want find a minimum enclosing circle for them.
+ * @param size - the size of the array points.
+ * @return 
+ */
 Circle findMinCircle(Point** points,size_t size){
 	return helper(points,size,{});
-	
 }
-
-
-
-
 
 #endif /* MINCIRCLE_H_ */
